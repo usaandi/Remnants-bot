16c37d84c322dc978bf92afab8554e888f3a3645
Kaur-Joonas Usai <kaurusai+git@gmail.com>
2025-08-05 22:24:47 +0300

    Global values

---
+++ config-globals.js
@@ -0,0 +1,18 @@
+
+// Individual exports
+export const API_URL = 'https://api.wiseoldman.net/v2'; //API url
+export const CHANNEL_ID = '1397129160148783188'; //Remants text channel
+export const STAFF_ROLE_ID = ['1382216510667227136', '1382287237818875924']; // STAFF ROLE IDS for Remnants
+export const MEMBERS_ROLE_ID = '1382240699801403423'; // Used for checking if has member role
+export const USER_AGENT = 'us_ai|discord_id: 1228569681355603981'; // Discord username to contact
+export const WOM_GROUP_ID = '12479'; // Remnants group id
+
+// Grouped export (optional)
+export const CONFIG = {
+  API_URL,
+  CHANNEL_ID,
+  STAFF_ROLE_ID,
+  MEMBERS_ROLE_ID,
+  WOM_GROUP_ID,
+  USER_AGENT
+};
